a247121c74933650552cdc8bce7a2480f009e2bd
Bilisim AKADEMISI16 <bilisim.akademisi16@arnavutkoy.local>
2021-06-09 18:54:53 +0300

    Proje dosyası ekle.

---
+++ com.bilisimakedemisi.csharp.consol/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace com.bilisimakedemisi.csharp.consol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("naber kadircan!");
+            #region 
+
+
+            #endregion
+
+            user _user = new user();
+
+
+            _user.MyPropert = 1;
+            _user.FullName = "kadircan.katirci";
+
+            user _user2 = _user;
+
+            _user2.FullName = "soner";
+
+
+
+            Console.WriteLine(_user.MyPropert);
+
+
+            Console.WriteLine(_user.FullName);
+
+            logger _logger = logger.Database;
+            if (_logger==logger.SMS)
+            {
+                //sms at
+            }
+               
+
+
+
+
+
+
+            
+        }
+    }
+}
---
+++ com.bilisimakedemisi.csharp.consol/user.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.bilisimakedemisi.csharp.consol
+{
+    class user
+    {
+        public int MyPropert{ get; set;}
+        public string FullName { get; set; }
+    }
+}
